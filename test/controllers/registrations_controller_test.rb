7fb188e6d6966660d96788943cd1de5f8cf6b533
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-13 19:05:10 +0200

    fixes test

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
 
   test "user signs up" do
-    post user_registration_path, params: {user: {email: 'test@example.org', password: '12345678'}}
+    post user_registration_path, params: {user: {email: 'test@example.org', password: '12345678', name: 'tester'}}
 
     assert response.successful?, "body was #{response.body}"
+    assert response.headers['Authorization'].start_with?('Bearer')
     assert_equal 'test@example.org', JSON.parse(response.body)['email'], response.body
   end
 end
