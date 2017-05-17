37f5b8757e5f5ba4fe0ca142c18b922756e21810
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-17 22:45:12 +0200

    simplifies loggin + makes confirmation not required

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < Devise::SessionsController
   skip_before_action :verify_signed_out_user, only: [:destroy]
 
   def create
-    if resource = warden.authenticate(auth_options)
+    resource = User.find_by email: params[:user].try(:[], :email)
+
+    if resource.valid_password? params[:user][:password]
       sign_in(resource_name, resource)
       render json: resource, status: 201
     else
       render json: {errors: {password: I18n.t('devise.failure.invalid')}}, status: 200
     end
-
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
 
   has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\z/
+
+  protected
+     def confirmation_required?
+       false
+     end
 end
