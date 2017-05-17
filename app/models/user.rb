c3402942e2e5c81b2d5ddd8b9527ceb421c30bff
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-17 23:40:37 +0200

    requires confirmation but allows signing in without confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,4 @@ class User < ApplicationRecord
   has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\z/
 
-  protected
-     def confirmation_required?
-       false
-     end
 end
