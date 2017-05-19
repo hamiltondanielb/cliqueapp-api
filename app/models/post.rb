e6f13a2303d636a4480eebd9478cbc0af7bbf4bc
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-19 18:01:30 +0200

    adds s3 region

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,8 @@ class Post < ApplicationRecord
       { :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }}},
     processors: lambda { |a| a.is_video? ? [ :transcoder ] : [ :thumbnail ] },
     storage: (Rails.env.production?? :s3 : :filesystem),
-    s3_credentials: Proc.new {|a| a.instance.s3_credentials}
+    s3_credentials: Proc.new {|a| a.instance.s3_credentials},
+    s3_region: ENV['S3_REGION']
 
   validates_attachment_content_type :media, content_type: [/\Aimage\/.*\z/, /\Avideo\/.*\z/]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
 
   has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" },
                     storage: (Rails.env.production?? :s3 : :filesystem),
-                    s3_credentials: Proc.new {|a| a.instance.s3_credentials }
+                    s3_credentials: Proc.new {|a| a.instance.s3_credentials },
+                    s3_region: ENV['S3_REGION']
+
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\z/
 
   has_many :posts, dependent: :destroy
