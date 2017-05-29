7b455e96ed9611ab4378d8c018bf3d6a72084c87
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-29 20:37:29 +0200

    adds guests to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :post
   has_one :location, dependent: :destroy, autosave:true
+  has_many :event_registrations
+  has_many :guests, through: :event_registrations, source: :user
 
   validates :start_time, :end_time, presence:true
+
+  def guest_count
+    event_registrations.count
+  end
 end
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :start_time, :end_time, :difficulty_level, :price, :women_only, :location
+  attributes :id, :start_time, :end_time, :difficulty_level, :price, :women_only, :location, :guest_count, :guests
 
   def location
     ActiveModelSerializers::SerializableResource.new object.location
