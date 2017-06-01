6f38b42428555c3be6987f1f8eb909da993cdef3
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-06-01 08:27:27 +0200

    destroys event registrations upon destroying an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :post
   has_one :location, dependent: :destroy, autosave:true
-  has_many :event_registrations
+  has_many :event_registrations, dependent: :destroy
   has_many :guests, through: :event_registrations, source: :user
 
   validates :start_time, :end_time, presence:true
