2900d4a4fc9362d01806d836cd1e753cdeb44e84
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-06-07 11:49:21 +0200

    removes registrations table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -79,15 +79,6 @@ ActiveRecord::Schema.define(version: 20170605113844) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "registrations", force: :cascade do |t|
-    t.integer "event_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_registrations_on_event_id"
-    t.index ["user_id"], name: "index_registrations_on_user_id"
-  end
-
   create_table "taggings", force: :cascade do |t|
     t.integer "tag_id"
     t.string "taggable_type"
