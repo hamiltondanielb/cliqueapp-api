eb5b7dca226313f8df537087515fd17d952c0a7f
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-06-15 20:36:49 +0200

    removes email from serialized user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :email, :created_at, :name, :profile_picture_url, :personal_website, :bio, :following_count, :follower_count, :follows, :likes, :post_count, :event_count, :registered_for_payouts, :instructor_terms_accepted
+  attributes :id, :created_at, :name, :profile_picture_url, :personal_website, :bio, :following_count, :follower_count, :follows, :likes, :post_count, :event_count, :registered_for_payouts, :instructor_terms_accepted
 
   def profile_picture_url
     object.profile_picture.url(:medium)
