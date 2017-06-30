14e6e70b14fa7a6d2dd25dc52da031bce328cb21
EricSSartorius <EricSSartorius@gmail.com>
2017-07-01 00:19:56 +0800

    adds user info to article

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,7 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :published_at, :user_id
+  attributes :id, :title, :body, :published_at, :user_id, :user
 
+  def user
+    ActiveModelSerializers::SerializableResource.new object.user
+  end
 end
