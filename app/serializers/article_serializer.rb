c1eccc300c15a2e1b216642e3d9e01731357969f
EricSSartorius <EricSSartorius@gmail.com>
2017-07-01 00:23:13 +0800

    like count added to article

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :published_at, :user_id, :user
+  attributes :id, :title, :body, :published_at, :user_id, :user, :like_count
 
   def user
     ActiveModelSerializers::SerializableResource.new object.user
